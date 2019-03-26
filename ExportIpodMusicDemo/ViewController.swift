32a7163dd05d572af8acdc3b2bd9a142321cfdfc
Vishal <vishaldodiya@logisticinfotech.co.in>
2019-03-26 15:34:20 +0530

    Commit player create and other misc changes

--- ExportIpodMusicDemo/ViewController.swift
+++ ExportIpodMusicDemo/ViewController.swift
@@ -58,37 +58,81 @@ class ViewController: UIViewController {
     
     //    let urlString = "https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview18/v4/9c/db/54/9cdb54b3-5c52-3063-b1ad-abe42955edb5/mzaf_520282131402737225.plus.aac.p.m4a"
     
+    @IBOutlet weak var viewPlayer: UIView!
+    @IBOutlet weak var constPlayerHeight: NSLayoutConstraint!
+    @IBOutlet weak var lblBeatName: UILabel!
+    @IBOutlet weak var lblCurruntTime: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var btnRestart: UIButton!
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var lblTotalTime: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    var overlayView : UIView? // This should be a class variable
+
+    var isPaused = false
     var audioPlayer : AVAudioPlayer?
     var arrFetchedMedia = NSMutableArray()
     
+    var timer = Timer()
+
     var arrFetchedDirMedia = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.title = "LI Export iPod Music"
-//        if((UserDefaults.standard.array(forKey: "MediaArray")) != nil){
-            if let unarchivedObject = UserDefaults.standard.object(forKey: "MediaArray") as? Data {
-                
-                self.arrFetchedMedia = (NSKeyedUnarchiver.unarchiveObject(with: unarchivedObject as Data) as? NSMutableArray)!
-            }
-//        }
+        //        if((UserDefaults.standard.array(forKey: "MediaArray")) != nil){
+        if let unarchivedObject = UserDefaults.standard.object(forKey: "MediaArray") as? Data {
+            
+            self.arrFetchedMedia = (NSKeyedUnarchiver.unarchiveObject(with: unarchivedObject as Data) as? NSMutableArray)!
+        }
+        //        }
+        constPlayerHeight.constant = 80
         self.tableView.reloadData()
         fetchImportedBeat()
         
     }
+    
+    
+    @IBAction func onClickRestart(_ sender: Any) {
+        if((audioPlayer) != nil){
+            timer.invalidate()
+            timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateVideoPlayerSlider), userInfo: nil, repeats: true)
+
+            if(audioPlayer!.isPlaying || isPaused ){
+                
+                audioPlayer!.stop()
+                audioPlayer!.currentTime =  0
+                btnPlay.setTitle("Pause", for: .normal)
+                audioPlayer!.play()
+            }else{
+                btnPlay.setTitle("Pause", for: .normal)
+                audioPlayer!.play()
+            }
+        }
+        
+    }
+    
+    @IBAction func onClickPlay(_ sender: Any) {
+        if((audioPlayer) != nil){
+            if(audioPlayer!.isPlaying){
+                audioPlayer!.pause()
+                isPaused = true
+                btnPlay.setTitle("Play", for: .normal)
+            }else{
+                audioPlayer!.play()
+                isPaused = false
+                btnPlay.setTitle("Pause", for: .normal)
+            }
+            
+        }
+        
+    }
+    
     @IBAction func onClickInsert(_ sender: Any) {
         self.presentPicker(sender)
     }
     
-    @IBAction func doGo (_ sender: Any!) {
-    }
-    
-    @IBAction func playMusic(_ sender: Any) {
-//        self.playmedia(fileName: "")
-        
-    }
     func presentPicker (_ sender: Any) {
         checkForMusicLibraryAccess {
             let picker = MPMediaPickerController(mediaTypes:.music)
@@ -133,9 +177,10 @@ class ViewController: UIViewController {
         }
     }
     func playmedia(fileName:URL) {
+        
         do {
             //7 - Insert the song from our Bundle into our AVAudioPlayer
-//            let fileName = (arrFetchedMedia[0] as! NSMutableDictionary).value(forKey: "storagePath") as! URL
+            //            let fileName = (arrFetchedMedia[0] as! NSMutableDictionary).value(forKey: "storagePath") as! URL
             audioPlayer = try AVAudioPlayer(contentsOf: fileName)
             //8 - Prepare the song to be played
             audioPlayer!.prepareToPlay()
@@ -155,9 +200,55 @@ class ViewController: UIViewController {
             print(songPlayerError)
         }
         if((audioPlayer) != nil){
+            
             audioPlayer!.play()
+            progressView.progress = 0.0
+            isPaused = false
+            btnPlay.setTitle("Pause", for: .normal)
+            timer.invalidate()
+            timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateVideoPlayerSlider), userInfo: nil, repeats: true)
+
+
+        }
+        
+    }
+    @objc func updateVideoPlayerSlider() {
+        if((audioPlayer) != nil){
+            if(audioPlayer!.isPlaying || isPaused){
+                let currentTimeInSeconds = audioPlayer!.currentTime
+                
+                let curruntMins = currentTimeInSeconds / 60
+                let CurruntSec = currentTimeInSeconds.truncatingRemainder(dividingBy: 60)
+                
+                let timeformatter = NumberFormatter()
+                timeformatter.minimumIntegerDigits = 2
+                timeformatter.minimumFractionDigits = 0
+                timeformatter.roundingMode = .down
+                guard let curruntMinsStr = timeformatter.string(from: NSNumber(value: curruntMins)), let curruntSecsStr = timeformatter.string(from: NSNumber(value: CurruntSec)) else {
+                    return
+                }
+                lblCurruntTime.text = "\(curruntMinsStr).\(curruntSecsStr)"
+                
+                let totalDuration = audioPlayer!.duration
+                let totalMins = totalDuration / 60
+                let totalSec = totalDuration.truncatingRemainder(dividingBy: 60)
+                
+                guard let totalMinsStr = timeformatter.string(from: NSNumber(value: totalMins)), let totalSecsStr = timeformatter.string(from: NSNumber(value: totalSec)) else {
+                    return
+                }
+                lblTotalTime.text = "\(totalMinsStr).\(totalSecsStr)"
+                
+                progressView.progress = Float(currentTimeInSeconds/totalDuration)
+                
+            }else{
+                timer.invalidate()
+                lblCurruntTime.text = "00:00"
+                btnPlay.setTitle("Play", for: .normal)
+            }
+          
         }
     }
+
 }
 
 extension ViewController :UITableViewDelegate,UITableViewDataSource{
@@ -171,12 +262,15 @@ extension ViewController :UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MediaCell
-        cell.lblMediaName.text = (((arrFetchedMedia[indexPath.row] as! NSMutableDictionary).value(forKey: "title")!) as! String)
-        cell.lblSizeName.text = (((arrFetchedMedia[indexPath.row] as! NSMutableDictionary).value(forKey: "size")!) as! String)
+        cell.lblMediaName.text = ((arrFetchedMedia[indexPath.row] as! [String:Any])["title"] as! String)
+        cell.lblSizeName.text = ((arrFetchedMedia[indexPath.row] as! [String:Any])["size"] as! String)
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.playmedia(fileName: ((arrFetchedMedia.object(at: indexPath.row) as! NSMutableDictionary) .value(forKey: "storagePath") as! URL))
+        
+        lblBeatName.text = ((arrFetchedMedia[indexPath.row] as! [String:Any])["title"] as! String)
+        constPlayerHeight.constant = 0
+        self.playmedia(fileName: ((arrFetchedMedia[indexPath.row] as! [String:Any])["storagePath"] as! URL))
     }
 }
 
@@ -184,28 +278,16 @@ extension ViewController : MPMediaPickerControllerDelegate {
     // must implement these, as there is no automatic dismissal
     
     func mediaPicker(_ mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
-        
-//        arrFetchedMedia.removeAllObjects()
-        let arrMedia = NSMutableArray()
-        
+        self.view.showBlurLoader()
         for tempItem in mediaItemCollection.items {
             
-            let dict = NSMutableDictionary()
+            var dict = [String:Any]()
             let item: MPMediaItem = tempItem
             let pathURL: URL? = item.value(forProperty: MPMediaItemPropertyAssetURL) as? URL
             if pathURL == nil {
                 print("Picking Error")
                 return
             }
-            
-            //            // get file extension andmime type
-            //            let str = pathURL!.absoluteString
-            //            let str2 = str.replacingOccurrences( of : "ipod-library://item/item", with: "")
-            //            let arr = str2.components(separatedBy: "?")
-            //            var mimeType = arr[0]
-            //            mimeType = mimeType.replacingOccurrences( of : ".", with: "")
-            
-            // Export the ipod library as .m4a file to local directory for remote upload
             let songAsset = AVURLAsset(url: pathURL!, options: nil)
             
             let tracks = songAsset.tracks(withMediaType: .audio)
@@ -253,14 +335,12 @@ extension ViewController : MPMediaPickerControllerDelegate {
                     default:
                         break;
                     }
-                    print("fileType \(fileType)")
-                    
                     let exportSession = AVAssetExportSession(asset: AVAsset(url: pathURL!), presetName: AVAssetExportPresetAppleM4A)
                     exportSession?.shouldOptimizeForNetworkUse = true
                     //                    exportSession?.outputFileType = AVFileType.m4a
-//                    exportSession?.outputFileType = AVFileType(rawValue: fileType! as String) ;
+                    //                    exportSession?.outputFileType = AVFileType(rawValue: fileType! as String) ;
                     exportSession?.outputFileType = AVFileType.m4a ;
-                   
+                    
                     var fileName = item.value(forProperty: MPMediaItemPropertyTitle) as! String
                     var fileNameArr = NSArray()
                     fileNameArr = fileName.components(separatedBy: " ") as NSArray
@@ -273,16 +353,25 @@ extension ViewController : MPMediaPickerControllerDelegate {
                     print("fileSizeString->\(item.fileSizeString)")
                     print("fileSize->\(item.fileSize)")
                     
-                    dict.setValue(fileType, forKey: "fileType")
-                    dict.setValue(ex, forKey: "extention")
-                    dict.setValue(fileName, forKey: "title")
-                    dict.setValue(outputURL, forKey: "storagePath")
-                    dict.setValue(item.fileSizeString, forKey: "size")
-                    dict.setValue(item.isCloudItem, forKey: "isCloudItem")
+                    dict = [
+                        "fileType":fileType as Any,
+                        "extention":ex as Any,
+                        "title":fileName,
+                        "storagePath":outputURL,
+                        "size":item.fileSizeString,
+                        "isCloudItem":item.isCloudItem
+                    ]
+
+                    let namePredicate = NSPredicate(format: "title like %@",dict["title"] as! String);
                     
-                    arrMedia.add(dict)
+                    let filteredArray = arrFetchedMedia.filter { namePredicate.evaluate(with: $0) };
+                    print("names = ,\(filteredArray)");
                     
-                    //Delete Existing file
+                    if(filteredArray.count == 0)
+                    {
+                        arrFetchedMedia.add(dict)
+                    }
+                    
                     do {
                         try FileManager.default.removeItem(at: outputURL)
                     } catch let error as NSError {
@@ -293,20 +382,22 @@ extension ViewController : MPMediaPickerControllerDelegate {
                     exportSession?.exportAsynchronously(completionHandler: { () -> Void in
                         
                         if exportSession!.status == AVAssetExportSession.Status.completed  {
+                           self.view.removeBluerLoader()
                             print("Export Successfull")
                             self.fetchImportedBeat()
-                            let arrArchive = NSKeyedArchiver.archivedData(withRootObject: arrMedia) as NSData
-
+                            let arrArchive = NSKeyedArchiver.archivedData(withRootObject: self.arrFetchedMedia) as NSData
+                            
                             let defaults = UserDefaults.standard
                             
                             defaults.set(arrArchive, forKey: "MediaArray")
-                            print("arr => \(arrMedia)")
-                            self.arrFetchedMedia = arrMedia
+                            print("arr => \(self.arrFetchedMedia)")
+                            
                             DispatchQueue.main.async {
-                            self.tableView.reloadData()
+                                self.tableView.reloadData()
                             }
                             
                         } else {
+                            self.view.removeBluerLoader()
                             print("Export failed")
                             print(exportSession!.error as Any)
                         }
@@ -320,6 +411,35 @@ extension ViewController : MPMediaPickerControllerDelegate {
     func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
         print("cancel")
         self.dismiss(animated:true)
+    }
+    
+}
+
+extension UIView{
+    func showBlurLoader(){
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.alpha = 0.8
+        blurEffectView.frame = self.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        let activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        activityIndicator.startAnimating()
+        
+        blurEffectView.contentView.addSubview(activityIndicator)
+        activityIndicator.center = blurEffectView.contentView.center
+        
+        self.addSubview(blurEffectView)
+    }
+    
+    func removeBluerLoader(){
+        DispatchQueue.main.async {
+            self.subviews.compactMap {  $0 as? UIVisualEffectView }.forEach {
+                $0.removeFromSuperview()
+            }
+        }
+        
     }
 }
 
